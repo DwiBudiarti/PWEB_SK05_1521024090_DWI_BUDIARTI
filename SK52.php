26bebf9146bcf1fa0e8e335da1cea780948fef98
DwiBudiarti <dbudiarty@gmail.com>
2016-11-26 18:00:37 +0700

    Add files via upload

---
+++ SK52.php
@@ -0,0 +1,27 @@
+<?php
+$alt = array
+
+(
+	array("1001","Sabun Lifebuoy","1.500"),
+	array("1002","Permen Blaster","5.600"),
+	array("1003","Pasta Gigi Pepsodent","4.560"),
+	array("1004","Madu Arbain","30.000"),
+	array("1005","Kecap ABC","7.250"),
+	array("1006","Saus Tomat ABC","6.700"),
+	array("1007","Gula Gulaku","8.900"),
+	array("1008","Rinso","7.100"),
+	array("1009","Super Pel","6.450"),
+	array("1010","Permen Tango","5.600")
+
+);
+echo "Kode Barang ".$alt[0][0].": Nama Barang ".$alt[0][1]." dengan harga : ".$alt[0][2]."<br>";
+echo "Kode Barang ".$alt[1][0].": Nama Barang ".$alt[1][1]." dengan harga : ".$alt[1][2]."<br>";
+echo "Kode Barang ".$alt[2][0].": Nama Barang ".$alt[2][1]." dengan harga : ".$alt[2][2]."<br>";
+echo "Kode Barang ".$alt[3][0].": Nama Barang ".$alt[3][1]." dengan harga : ".$alt[3][2]."<br>";
+echo "Kode Barang ".$alt[4][0].": Nama Barang ".$alt[4][1]." dengan harga : ".$alt[4][2]."<br>";
+echo "Kode Barang ".$alt[5][0].": Nama Barang ".$alt[5][1]." dengan harga : ".$alt[5][2]."<br>";
+echo "Kode Barang ".$alt[6][0].": Nama Barang ".$alt[6][1]." dengan harga : ".$alt[6][2]."<br>";
+echo "Kode Barang ".$alt[7][0].": Nama Barang ".$alt[7][1]." dengan harga : ".$alt[7][2]."<br>";
+echo "Kode Barang ".$alt[8][0].": Nama Barang ".$alt[8][1]." dengan harga : ".$alt[8][2]."<br>";
+echo "Kode Barang ".$alt[9][0].": Nama Barang ".$alt[9][1]." dengan harga : ".$alt[9][2]."<br>";
+?>
